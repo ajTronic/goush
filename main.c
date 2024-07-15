6ddde0cb2b217746f3ec6b0e826f14b54970f72b
ajTronic <emailaj@email.com>
2024-07-15 18:43:47 +0200

    "finished"

--- main.c
+++ main.c
@@ -5,26 +5,70 @@
 #include <unistd.h>
 
 // returns the arg list for a command
-char** parse_command(char command[]) {
-  char** result = NULL;
-  char* curr = strtok(command, " "); // pointer to current token
+char **split_command(char command[]) {
+  char **result = NULL;
+  char *curr = strtok(command, " "); // pointer to current token
   int num_spaces = 0;
 
   while (curr) { // while curr != NULL
     // add 1 char to result
-    result = realloc(result, sizeof(char*) * (++num_spaces)); 
-    if (result == NULL) exit(0); // realoc failed
+    result = realloc(result, sizeof(char *) * (++num_spaces));
+    if (result == NULL)
+      exit(0); // realoc failed
 
-    result[num_spaces-1] = curr; // set that char to the current token
-    curr = strtok(NULL, " "); // get next token
+    result[num_spaces - 1] = curr; // set that char to the current token
+    curr = strtok(NULL, " ");      // get next token
   }
 
   // append null-terminator
-  result = realloc(result, sizeof(char*) * (num_spaces+1)); 
-  if (result == NULL) exit(0); // realloc failed
+  result = realloc(result, sizeof(char *) * (num_spaces + 1));
+  if (result == NULL)
+    exit(0); // realloc failed
   result[num_spaces] = 0;
 
   return result;
+}
+
+void exec_command(char **arg_list) {
+  char *path = arg_list[0];
+
+  // handle builtin commands
+  if (strcmp(path, "cd") == 0)
+    chdir(arg_list[1]);
+  else if (strcmp(path, "exit") == 0)
+    exit(0);
+  else if (strcmp(path, "help") == 0)
+    puts("Yo! This is a shell. If you're still stuck, here's some advice: git gud bro.");
+  else if (strcmp(path, "..") == 0)
+    chdir("..");
+  // execute 3rd party command, e.g. clear
+  else {
+    // create a clone of this process
+    pid_t p = fork();
+
+    // fork failed
+    if (p < 0) {
+      perror("fork failed");
+      exit(0);
+    }
+
+    // parent
+    else if (p > 0) {
+      wait(NULL); // wait for child to finish
+      return;     // continue parent process
+    }
+
+    // child
+    else {
+      execvp(path, arg_list);
+
+      // this means execvp failed, as it did not change the process
+      printf("unknown command\n");
+
+      // kill child process
+      exit(0);
+    }
+  }
 }
 
 int main() {
@@ -37,23 +81,12 @@ int main() {
     fgets(input, sizeof(input), stdin);
     input[strcspn(input, "\n")] = '\0'; // remove newline
 
-    // execute command
-    pid_t p = fork();
-    if (p < 0)
-      perror("fork failed");
-    else if (p == 0) {
-      // child
-      char **arg_list = parse_command(input);
-      execvp(arg_list[0], arg_list);
+    // execute
+    char **arg_list = split_command(input);
+    exec_command(arg_list);
 
-      printf("unknown command\n");
-      exit(0);
-    } else {
-      // parent
-      wait(NULL); // wait for command to finish
-    }
+    free(arg_list);
   }
 
   return 0;
 }
-
